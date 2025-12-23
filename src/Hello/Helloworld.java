4f249021dfa956e19f1dc6b6d662650eccff82f9
gokul <krishnapriya.gokulakrishnan@utb.ecutbildning.se>
2025-12-23 10:42:54 +0100

    How are you

--- src/Hello/Helloworld.java
+++ src/Hello/Helloworld.java
@@ -3,5 +3,6 @@ package Hello;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("How are you");
     }
 }
